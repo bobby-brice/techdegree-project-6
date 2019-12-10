1414c7ef7bc6ca1ab3b898d622d707635c7aa93b
Bobby Brice <bobby.brice@gmail.com>
2019-12-09 19:07:52 -0500

    changed typeStrings

--- public/js/typed.js
+++ public/js/typed.js
@@ -1,5 +1,5 @@
 const typed = new Typed('.typedStrings', {
-  strings: ['Dog Lover', 'Full Stack Developer', 'Photographer', 'Traveler'],
+  strings: ['Dog Lover', 'Full Stack Developer', 'Photographer', 'Traveller'],
   typeSpeed: 60,
   backSpeed: 40,
   cursor: false,
